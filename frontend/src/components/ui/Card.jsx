a3ea41c34035dc078234586fc19d85dea9c1c237
Hadji <hadjidiallo00224@gmail.com>
2026-08-10 17:58:26 +0000

    chore(frontend): remove deprecated application

--- frontend/src/components/ui/Card.jsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '../../lib/utils.js'
-
-export default function Card({ children, className, as: Component = 'div', ...props }) {
-  return <Component className={cn('rounded-lg border border-fifow-border bg-white shadow-card', className)} {...props}>{children}</Component>
-}
--- frontend/src/lib/formatters.js
+++
@@ -1,9 +0,0 @@
-export function formatGNF(value) {
-  return new Intl.NumberFormat('fr-GN', {
-    maximumFractionDigits: 0,
-  }).format(value) + ' GNF'
-}
-
-export function shortLocation(location) {
-  return location?.replace('Conakry, ', '') ?? ''
-}
